0b8a2ae3d818953f91c0dc176a0cb121c20277f2
huiqit <46876000+huiqit@users.noreply.github.com>
2019-07-07 12:35:15 -0400

    Update 4. k-th smallest number with 3,5,7 factors.java

--- src/P6.3 Dijkstra/4. k-th smallest number with 3,5,7 factors.java
+++ src/P6.3 Dijkstra/4. k-th smallest number with 3,5,7 factors.java
@@ -1,4 +1,26 @@
 /*
 Find the Kth smallest number s such that s = 3 ^ x * 5 ^ y * 7 ^ z, x > 0 and y > 0 and z > 0, x, y, z are all integers.
-
+每次变化的都可在原s上：*3, *5, *7
 */
+
+public long kth(int k) {
+  PriorityQueue<Long> minHeap = new PriorityQueue<>();
+  Set<Long> set = new HashSet<>();
+  minHeap.offer(3*5*7l);
+  set.add(3*5*7l);
+  
+  while(k > 1) {
+    long curr = minHeap.poll();
+    if(set.add(curr*3)) {
+      minHeap.offer(curr*3);
+    }
+    if(set.add(curr*5)) {
+      minHeap.offer(curr*5);
+    }
+    if(set.add(curr*7)) {
+      minHeap.offer(curr*7);
+    }
+    k--;
+  }
+  return minHeap.peek();
+}
