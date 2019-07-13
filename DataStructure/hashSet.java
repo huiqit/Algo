a663ee23fc7b73ff79123826c89774c9985e6406
huiqit <46876000+huiqit@users.noreply.github.com>
2019-07-12 21:27:36 -0400

    Update hashSet.java

--- DataStructure/hashSet.java
+++ DataStructure/hashSet.java
@@ -1,1 +1,19 @@
-
+//is backed up by a hashMap instance
+class HashSet<K> {
+  private HashMap<K, Object> map;
+  
+  private static final Object PRESENT = new Object();
+  
+  public HashSet() {
+    map = new HashMap<Key, Object>();
+  }
+  
+  public boolean contains(K key) {
+    return map.containsKey(key);
+  }
+  
+  public boolean add(K key) {
+    return map.put(key, PRESENT) == null;
+  }
+  ...
+}
