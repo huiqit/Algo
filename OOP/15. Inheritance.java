84a053cb95eb991f52fe61627439750b0edf6dc3
huiqit <46876000+huiqit@users.noreply.github.com>
2019-08-11 18:25:28 -0400

    Update 15. Inheritance.java

--- OOP/15. Inheritance.java
+++ OOP/15. Inheritance.java
@@ -1,3 +1,43 @@
+class Animal {
+  String name;
+  void makeSound() {}
+    System.out.println("animal sound");
+}
+class Dog extends Animal {
+  
+  @Override 
+  void makeSound() {
+    System.out.println("wangwang");
+  }
+  
+}
+
+class Cat extends Animal {
+  
+  @Override 
+  void makeSound() {
+    System.out.println("miaomiao");
+  }
+  
+}
+
+public class Polymophysm {
+  public static void main(String[] args) {
+    System.out.println("Hello");
+    new Dog();
+    //怎么访问它
+    Dog d = new Dog();
+    d.name = "wangwang"; 
+    d.makeSound(); // bind the method in class Dog
+    Animal a = new Dog(); // new 谁就绑定谁 多态
+    // 只能访问Animal的那部分属性
+    // 非要访问dog的属性：
+    Dog d = (Dog)a;
+    d.swimmingSpeed = 3; 
+    
+    
+  }
+}
 
 
 
