f76d50496816ab0cdb37579f0cf3e67bcf4503c8
huiqit <46876000+huiqit@users.noreply.github.com>
2019-08-20 09:53:50 -0400

    Update BoundedQueue.java

--- DataStructure/BoundedQueue.java
+++ DataStructure/BoundedQueue.java
@@ -1,1 +1,56 @@
-
+public class BoundedQueue {
+  int head;
+  int tail;
+  int size;
+  Integer[] array;
+  
+  public BoundedQueue(int cap) {
+    head = tail = 0;
+    size = 0;
+    array = new Integer[cap];
+  }
+  // head poll()
+  // tail offer()
+  public boolean offer(Integer ele) {
+    if(size == array.length) {
+      return false;
+    }
+    array[tail] = ele;
+    tail = (tail+1) % array.length;
+    // tail = tail + 1 == array.length ? 0 : tail + 1;
+    size++;
+    return true;
+  }
+  
+  public Integer poll() {
+    if(size == 0) {
+      return null;
+    }
+    Integer res = array[head];
+    head = (head+1) % array.length;
+    size--;
+    return res;
+  }
+  
+  public Integer peek() {
+    if(size == 0) {
+      return null;
+    }
+    return array[head];
+  }
+  
+  public int size() {
+    return size;
+  }
+  
+  public boolean isEmpty() {
+    return size == 0;
+  }
+  
+  public boolean isFull() {
+    return size == array.length;
+  }
+}
+    
+    
+    
