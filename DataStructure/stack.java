75db674a7904a5d5a6d1e147c668f2052bea527d
huiqit <46876000+huiqit@users.noreply.github.com>
2019-08-20 09:30:58 -0400

    Update stack.java

--- DataStructure/stack.java
+++ DataStructure/stack.java
@@ -1,1 +1,29 @@
-
+public class Stack {
+  private ListNode head;
+  public Stack() {
+    head = null;
+  }
+  
+  public Integer pop() {
+    if(head == null) {
+      return null;
+    }
+    ListNode prev = head;
+    head = head.next;
+    prev.next = null;
+    return prev.value;
+  }
+  
+  public Integer peek() {
+    if(head == null) {
+      return null;
+    }
+    return head.value;
+  }
+  
+  public void push(int ele) {
+    ListNode node = new ListNode(ele);
+    node.next = head;
+    head = node;
+  }
+}
