355b9556a47b4184843b7c8f06758051ce0ecd8b
huiqit <46876000+huiqit@users.noreply.github.com>
2019-08-26 19:44:31 -0400

    Update Singleton.java

--- Concurrency/Singleton.java
+++ Concurrency/Singleton.java
@@ -1,8 +1,20 @@
-
-// 1. set to private constructor
-// 2. out of class cannot call private constructor, but in the class can call it
-// 3. static? if non-static, should use an instance to call it
-
+public class Test {
+    public static void main(String[] args) {
+        Single a = new Single("kaijun");
+        Single b = new Single("chenyu");
+       Single x = Single.getInstance();
+       MySingle s = MySingle.getInstance();
+       MySingleton chou = MySingleton.getInstance();
+       System.out.println(x.name);
+       System.out.println(s.name);
+       System.out.println(chou.name);
+    }
+}
+/*
+1. set to private constructor
+2. out of class cannot call private constructor, but in the class can call it
+3. static? if non-static, should use an instance to call it
+*/
 class Single {
     String name;
 
@@ -76,3 +88,8 @@ class Singleton {
         }
         return instance;
     }
+}
+
+
+
+
