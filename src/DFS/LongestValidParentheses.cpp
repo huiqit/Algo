4c81fdb02cae1473958937a4630de9b72b22211d
huiqit <46876000+huiqit@users.noreply.github.com>
2019-10-04 16:08:53 -0400

    Update LongestValidParentheses.cpp

--- src/DFS/LongestValidParentheses.cpp
+++ src/DFS/LongestValidParentheses.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+  public: 
+  int longest(string s) {
+    int res = 0;
+    stack<int> st;
+    for(int i = 0; i < s.size(); i++) {
+      if(s[i] == '(') {
+        st.push(i);
+      } else {
+        if(s[st.top()] == '(') {
+          st.pop();
+          res = max(i - (st.empty() ? -1 : st.top()), res);
+        } else {
+          st.push(i);
+        }
+      }
+    }
+    return res;
+  }
+}
