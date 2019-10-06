989fb7aede263d3f6efa380b8a0cec3c176011a8
huiqit <46876000+huiqit@users.noreply.github.com>
2019-10-05 22:32:10 -0400

    Update shuffle.cpp

--- src/Stream|Sampling Problem/shuffle.cpp
+++ src/Stream|Sampling Problem/shuffle.cpp
@@ -1,1 +1,29 @@
-
+/*
+C++ swap is an in-place function 
+array.size()
+rand(): 0 ~ RAND_MAX
+rand() % range + initial value
+initialize random seed: srand(time(NULL))
+*/
+class Solution {
+  public:
+    vector<int> numbers;
+    Solution(vector<int> nums) {
+      numbers = nums; //this -> nums = nums;
+    }
+    
+    vector<int> reset() {
+      return numbers;
+    }
+  
+    vector<int> shuffle() {
+      vector<int> array = numbers; //vector<int> result(numbers);
+      //srand(time(NULL)); 加上这句就过不了 ？？
+      for(int i = 0; i < array.size(); i++) { 
+        int candidate = i + rand()%(array.size() - i);
+        swap(array[i], array[candidate]); 
+      }
+      return array;
+    }
+}
+ 
