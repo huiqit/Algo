8c73402bf1404983b0f00511ecace63dee121cc5
huiqit <46876000+huiqit@users.noreply.github.com>
2019-10-06 11:43:43 -0400

    Update reserviorSampling.cpp

--- src/Stream|Sampling Problem/reserviorSampling.cpp
+++ src/Stream|Sampling Problem/reserviorSampling.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+  vector<int> reserviorSampling(vector<int> input, int k) {
+    vector<int> res;
+    srand(time(NULL)); 
+    for(int i = 0; i < k; i++) {
+      res.push_back(input[i]);
+    }
+    for(int i = k; i < input.size(); i++) {
+      int candidate = rand() % i;
+      if(i < k) {
+        res[i] = input[i];
+      }
+    }
+    return res;
+  }
+}
