ef0892b0996dd7790785a9da29e86452b85a414a
huiqit <46876000+huiqit@users.noreply.github.com>
2019-10-17 12:51:50 -0400

    Update a^b.java

--- src/Recursion/a^b.java
+++ src/Recursion/a^b.java
@@ -1,19 +1,20 @@
 Q1.1 a^b
 /*
-模块化你的逻辑
-helper function就做helper function的事，error msg在主程序里处理
-6种情况，合并，分类
-注意强制转化应发生在分母上（运算之前），而非之后
+      a<0 a=0 a>0
+b<0   
+b=0
+b>0
+
 */
-public double power(int a, int b) {
-  if(a == 0) {
+public double power(int x, int n) {
+  if(x == 0) {
     return 0;
   }
   
-  if(b >= 0) {
-    return powerHelper(a, b);
+  if(n >= 0) {
+    return powerHelper(x, n);
   } else {
-    return 1/(double)powerHelper(a, -b); //此处虽然传入b结果一样，但意义不同，且违背了我们定义helper func的语意
+    return 1/(double)powerHelper(x, -n); //此处虽然传入b结果一样，但意义不同，且违背了我们定义helper func的语意
   }
 }
 
