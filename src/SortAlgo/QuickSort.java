610a63763133353235e19f259cc7be651ff8af06
huiqit <46876000+huiqit@users.noreply.github.com>
2019-10-16 21:09:51 -0400

    Update QuickSort.java

--- src/SortAlgo/QuickSort.java
+++ src/SortAlgo/QuickSort.java
@@ -1,1 +1,30 @@
-
+// quick sort
+class QuickSort {
+ public void quickSort(int[] array) {
+   if(array == null || array.length == 0) {
+     return;
+   }
+   sort(array, 0, array.length - 1);
+ }
+ private void sort(int[] array, int left, int right) {
+   if(left >= right) {
+     return;
+   }
+   Random rand = new Random();
+   int pivotIndex = left + rand.nextInt(right - left + 1);
+   swap(array, pivotIndex, right);
+   int i = left;
+   int j = right - 1;
+   while(i <= j) {
+     if(array[i] < array[right]) {
+       i++;
+     } else {
+       swap(array, i, j);
+       j--;
+     }
+   }
+   swap(array, right, i);
+   sort(array, left, i-1);
+   sort(array, i+1, right);
+ }
+}
