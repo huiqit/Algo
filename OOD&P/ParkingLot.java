98a17bacdca9d87af6eab6bcbc2f83c1a31753eb
huiqit <46876000+huiqit@users.noreply.github.com>
2019-10-22 12:53:05 -0400

    Update ParkingLot.java

--- OOD&P/ParkingLot.java
+++ OOD&P/ParkingLot.java
@@ -1,1 +1,40 @@
+// Assume: multiple levels, several size Vehicles
 
+public class ParkingLot {
+  // multiple levels
+  private final Level[] levels;
+  
+  public ParkingLot(int numOfLevels, int numsOfSpotsPerLevel) {
+    
+  }
+  public boolean hasSpot(Vehicle v) {
+    
+  }
+  
+  public boolean park(Vehicle v) {
+    
+  }
+  
+  public boolean leave(Vehivle v) {
+    
+  }
+}
+
+class Level {
+  // has many spots --> a list of spots
+  private final List<ParkingSpot> spots;
+  
+  Level(int numOfSpots) {
+    List<ParkingSpot> list = new ArrayList<>(numOfSpots);
+    ...
+    spots = Collections.unmodifiableList(list);
+  }
+  boolean hasSpot(Vehicle v) {
+    for(ParkingSpot s : spots) {
+      if(s.fit(v)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
