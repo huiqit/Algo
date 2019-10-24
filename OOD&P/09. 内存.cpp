59f3658e8abb98eb0d87313b469ecd3b498ae5ec
huiqit <46876000+huiqit@users.noreply.github.com>
2019-10-24 14:26:34 -0400

    Update 09. 内存.cpp

--- OOD&P/09. 内存.cpp
+++ OOD&P/09. 内存.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream> 
+using namespace std;
+
+class Shape {
+public: 
+  string* name;
+  float area;
+public:
+  Shape() {
+    name = new string("shape");
+    cout << "shape" << endl;
+  }
+};
+
+int main() {
+  Shape* s = new Shape();
+  s-> area = 1.0;
+  cout << s->name << endl;
+  delete s;
+}
+
